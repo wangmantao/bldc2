3bf04fee3f69ab7863d3ac109c3d93baf6fd31d5
wangmantao <wangmantao@163.com>
2017-12-27 23:14:22 +0800

    预定位debug pass

--- Code/main.c
+++ Code/main.c
@@ -10,20 +10,20 @@ FOR BLDC 2017
 /* ---------- 定义常量 --------------*/
 #define STM8_FREQ_MHZ 16 
 #define PWM_FREQUENCY 16000 		//PWM频率16K, 将被计算出pwm值,写入PWMPH/L
-#define PWMOUT 15 			//预定位（proLoc）占空比
+#define PWMOUT 20 			//预定位（proLoc）占空比
 #define PWMOUT_2 15 			//启动（）占空比
-#define DOWN_A_OFF set_P00
-#define DOWN_B_OFF set_P11
-#define DOWN_C_OFF set_P03
-#define DOWN_A_ON clr_P00
-#define DOWN_B_ON clr_P11
-#define DOWN_C_ON clr_P03
+#define DOWN_A_ON set_P00
+#define DOWN_B_ON set_P11
+#define DOWN_C_ON set_P03
+#define DOWN_A_OFF clr_P00
+#define DOWN_B_OFF clr_P11
+#define DOWN_C_OFF clr_P03
 
 /* ------- 定义变量 --------------*/
 unsigned int i = 0;
 unsigned char workstep = 0;
 unsigned int pwmduty = 0;
-static const unsigned int pwm = ((unsigned int)((STM8_FREQ_MHZ * (unsigned long)1000000)/PWM_FREQUENCY));
+static const unsigned int pwm = ((unsigned int)((STM8_FREQ_MHZ * (unsigned long)1000000)/PWM_FREQUENCY) - 1 );
 const unsigned char PWM_MARSK_TABLE[6]={0x01, 0x01, 0x10, 0x10, 0x04, 0x04};
 /* ---------- 定义函数 --------------*/
 // 系统时钟配置: 内部16M
@@ -34,32 +34,26 @@ void clkConf(){
 void ioConf(){
 	PWM0_P12_OUTPUT_ENABLE; 	// upA
 	PWM4_P01_OUTPUT_ENABLE; 	// upB
-	PWM3_P00_OUTPUT_ENABLE; 	// upC
+	PWM2_P10_OUTPUT_ENABLE; 	// upC
 
-	P00_PushPull_Mode; set_P00;	//downA
-	P11_PushPull_Mode; set_P11;	// downB
-	P03_PushPull_Mode; set_P03;	// downC
-}
-
-void setDuty(unsigned int dutyvalue){
-	/* 可变值 */
-	PWM0H =(unsigned char) dutyvalue >> 8;           // set duty 
-	PWM0L =(unsigned char) dutyvalue; 
+	P00_PushPull_Mode; 	//downA
+	P11_PushPull_Mode; 	// downB
+	P03_PushPull_Mode; 	// downC
 }
 
 void pwmConf(){
 	set_CLRPWM;     		// clrear pwm counter
 					//PWM_CLOCK_DIV_8;	// sysClk / 8 for pwm
 	PWM_GP_MODE_ENABLE;		// use group mode
-	PWMPH = (unsigned char) pwm >> 8;  // set pwm frequnce	
+	PWMPH = (unsigned char) (pwm >> 8);  // set pwm frequnce	
 	PWMPL = (unsigned char) pwm; 
 	
-	PMD = 0X00;			// wen masked, 00: ground / FF: vcc
+	PMD = 0XFF;			// wen masked, 00: ground / FF: vcc
 	PWM_OUTPUT_ALL_NORMAL;		// set on_duty is hight/low & _INVERSE
 }
 
 void pwmStart(){
-	set_EPWM;    		//Enable PWM interrupt
+	//set_EPWM;    		//Enable PWM interrupt
 	set_EA;
 	set_LOAD;
 	set_PWMRUN;
@@ -74,9 +68,8 @@ void setCommutation(unsigned char workstep, unsigned int dutyv){
 	    DOWN_B_OFF;
 	if(workstep!=1&&workstep!=2)
 	    DOWN_C_OFF;
-
-	setDuty(dutyv);				// set duty value in each commuation
-
+	PWM0H =(unsigned char) (dutyv>> 8);           // set duty 
+	PWM0L =(unsigned char) dutyv; 
 	if(workstep==0) {			//AB
 		DOWN_B_ON;
 	}
@@ -96,14 +89,14 @@ void setCommutation(unsigned char workstep, unsigned int dutyv){
 	  	DOWN_B_ON;
 	}
 	
-	PMEN = ~ PWM_MARSK_TABLE[workstep];	// one chanel pwm open
+	PMEN = ~ (PWM_MARSK_TABLE[workstep]);	// one chanel pwm open
 }
 
 void preLoc(){
 	workstep = 5;		
 	pwmduty = pwm * PWMOUT /100;      // duty cicy
 	setCommutation(workstep, pwmduty);
- 	for(i=0; i<1000; i++); 	// delay
+ 	//for(i=0; i<100; i++); 	// delay
 }
 
 /* 定义功能函数 */
@@ -115,8 +108,8 @@ void main(){
 	clkConf();
 	ioConf();
 	pwmConf();
-	pwmStart();
 	preLoc();			// the time is not sure
+	pwmStart();
 	while (1){
 	}
 }
