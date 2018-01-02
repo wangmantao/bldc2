6d4d21f13ec662b4ab1b1ed60ecd800924658455
wangmantao <wangmantao@163.com>
2018-01-02 13:47:08 +0800

    加速前的备份

--- Code/main.c
+++ Code/main.c
@@ -10,8 +10,8 @@ FOR BLDC 2017
 /* ---------- 定义常量 --------------*/
 #define STM8_FREQ_MHZ 16 
 #define PWM_FREQUENCY 16000 		//PWM频率16K, 将被计算出pwm值,写入PWMPH/L
-#define PWMOUT 80 			//预定位（proLoc）占空比
-#define PWMOUT2 5 			//启动（）占空比
+#define PWMOUT 30 			//预定位（proLoc）占空比
+#define PWMOUT2 30 			//启动（）占空比
 /*
 #define DOWN_A_ON set_P00			// this for high level avalable
 #define DOWN_B_ON set_P11
@@ -29,9 +29,9 @@ FOR BLDC 2017
 #define DOWN_C_OFF set_P03
 
 /* ------- 定义变量 --------------*/
-unsigned int i = 0;
-unsigned char workstep = 0;
-unsigned char startOk = 0;
+static unsigned char startOk = 0;
+static unsigned char workstep = 0;
+static	unsigned int actCount = 0;
 // PWM 占空比的寄存器值 （PWM0H/PWML)
 static unsigned int duty = 0;
 // PWM 频率的寄存器值 （PWMPH/PWMPL)
@@ -57,41 +57,38 @@ void ioConf(){
 }
 
 void pwmConf(){
-	set_CLRPWM;     		// clrear pwm counter
-							//PWM_CLOCK_DIV_8;	// sysClk / 8 for pwm
+	set_CLRPWM;     		// clrear pwm counter //PWM_CLOCK_DIV_8;	// sysClk / 8 for pwm
 	PWM_GP_MODE_ENABLE;		// use group mode
 	PWMPH = (unsigned char) (pwm >> 8);  // set pwm frequnce	
 	PWMPL = (unsigned char) pwm; 
-	
-	/* 上管高电平有效，平时不动作是，因为低电平 */
-	PMD = 0X00;				// wen masked, 00: ground / FF: vcc (note: upFET-off)
-	PWM_OUTPUT_ALL_NORMAL;		// set on_duty is [default]hight:low ( [default]NORMAL: INVERSE )
+	/* 上管高电平有效，平时不动作为低电平 */
+	PMD = 0X00;				//(PWM掩码数据) wen masked, 00: ground / FF: vcc (note: upFET-off)
+	PWM_OUTPUT_ALL_NORMAL;	//(占空输出的极性) set on_duty is [default]hight:low ( [default]NORMAL: INVERSE )
 }
 
 void pwmStart(){
-	//set_EPWM;    			//Enable PWM interrupt
+	set_EPWM;    			//Enable PWM interrupt
 	set_EA;
 	set_LOAD;
 	set_PWMRUN;
 }
 
 void pwmStop(){
-	//clr_EPWM;    			//Enable PWM interrupt
+	clr_EPWM;    			//Enable PWM interrupt
 	clr_EA;
-	clr_LOAD;
 	clr_PWMRUN;
 }
 
 void setDuty(){
 	PWM0H =(unsigned char) (duty >> 8);    // set duty 
 	PWM0L =(unsigned char) duty; 
-	while( !(LOAD == 0X00) );				// whait LOAD reset 0 by self
+	//while( !(LOAD == 0X00) );				// whait LOAD reset 0 by self
 	set_LOAD;
 }
 
 //void setCommutation(unsigned char workstep, unsigned int dutyv){
 void setCommutation(unsigned char workstep){
-	PMEN = 0xFF;				// pwm output all off
+	//PMEN = 0xFF;				// pwm output all off
 
 	if(workstep!=3&&workstep!=4)
 	    DOWN_A_OFF;
@@ -100,7 +97,7 @@ void setCommutation(unsigned char workstep){
 	if(workstep!=1&&workstep!=2)
 	    DOWN_C_OFF;
 
-	setDuty(duty);
+	setDuty();
 
 	if(workstep==0) {			//AB
 		DOWN_B_ON;
@@ -126,49 +123,53 @@ void setCommutation(unsigned char workstep){
 
 void preLoc(){
 	workstep = 5;		
-	//duty=pwm*PWMOUT/100;      	// duty cicy(location stage)
+	duty=pwm*PWMOUT/100;      	// duty cicy(location stage)
 	duty =(unsigned int) (pwm*PWMOUT/100);      	// duty cicy(location stage)
-	//setCommutation(workstep, pwmduty);
+	//setDuty();
 	setCommutation(workstep);
- 	//for(i=0; i<16000; i++); 	// delay
 }
 
 unsigned char  bldcStart(){
-	unsigned int actCount;
+	actCount = 0;
 	startOk = 0;
 	do{					   // only one pass, not need do this
 		workstep++; actCount++;
 		if(workstep >=6 ) workstep = 0;
 		setCommutation(workstep);	
- 		for(i=0; i<6000; i++); 	// delay
-	}while(actCount < 200 && startOk == 0);   // action not enough, and start fail 
+		Timer0_Delay1ms(20);
+		//Timer0_Delay100us(50);
+	}while(actCount < 5000 && startOk == 0);   // action not enough, and start fail 
 
-	if(actCount >= 200){
+	if(actCount >= 5000){
 		return 0; 	// ng
 	}	
 	return 1;		// ok
 }
 
+void keepAllOff(){
+	clr_P12; clr_P01; clr_P10; 	// upA  upB  upC OFF
+	duty = 0x00; setDuty(); 	// keep low for PWM mode start
+	DOWN_A_OFF; DOWN_B_OFF; DOWN_C_OFF;
+	pwmStop();
+}
+
 /* 定义功能函数 */
 void main(){
 	Set_All_GPIO_Quasi_Mode;
-	clr_P12; 	// upA
-	clr_P01; 	// upB
-	clr_P10; 	// upC
-
-	Timer0_Delay1ms(800);
-
+	keepAllOff();
 	clkConf();
 	ioConf();
-	//pwmConf();
-	//preLoc();			
-	//pwmStart();
-	Timer0_Delay1ms(200);		// preLocation delay 
-	//pwmStop();
+	pwmConf();
+	Timer0_Delay1ms(50);
+	preLoc();			
+	pwmStart();
+	Timer0_Delay1ms(500);		// preLocation delay 
+	pwmStop();
 	duty =(unsigned int)(pwm*PWMOUT2/100);      	// duty cicy(location stage)
-	//pwmStart();
+	setDuty();
+	pwmStart();
 	if(bldcStart() == 0){
-		//pwmStop();
+		keepAllOff();
 		while(1);		// no started to stop all
 	}	
 	while (1){
