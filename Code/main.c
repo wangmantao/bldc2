7af65b74f18337fa2b39569540517daeb11afc87
wangmantao <wangmantao@163.com>
2018-02-01 01:34:07 +0800

    CS SC DIR OK

--- Code/main.c
+++ Code/main.c
@@ -10,8 +10,8 @@
    LC   P03
 
    EA   P30 AIN1 -> P05 AIN4
-   EB   P07 AIN2
-   EC   P06 AIN3
+   EB   P07 AIN2 => P06 AIN3 (PCB 设计适应)
+   EC   P06 AIN3 => P07 AIN2 (PCB 设计适应)
    MV   P17 AIN0
 
    HV   (P17 AIN0) -> P30 AIN1 方向信号
@@ -27,7 +27,7 @@
 
 /* ---------- 定义常量 --------------*/
 #define STM8_FREQ_MHZ 16 
-#define PWM_FREQUENCY 16000 		//PWM频率16K, 将被计算出pwm值,写入PWMPH/L
+#define PWM_FREQUENCY 18000 		//PWM频率16K, 将被计算出pwm值,写入PWMPH/L
 //#define PWMOUT 15 			//预定位（proLoc）占空比
 
 /* ------------------------------------
@@ -58,17 +58,18 @@
 */
 
 /* ------- 定义变量 --------------*/
-static bit joSwitch = 0;			// 奇偶开关，偶取中点电压（先），奇取端电压
 static bit zOk= 0;            // 奇偶开关，偶取中点电压（先），奇取端电压
 static bit demag = 0;
-static bit dir = 1;
-static char startLevel = 2;            // 0  启动前
+static bit dir = 0;
+static char adcSwitch = 0;			// adc开关，0:中点电压（先），1:取端电压, 3:母线电流检测
+static char startLevel = 0;            // 0  启动前
 static unsigned int i = 0;
 static unsigned char startOk = 0;
 static unsigned char workstep = 0;
 static unsigned int actCount = 0;
-static unsigned int hv = 0;     // 中点电压
+static unsigned int hv = 0;     // 中点电压 (在startLevel小于1，定方向时，存临时值)
 static unsigned int ev = 0;     // 悬空端电压
+static unsigned int ocpv = 0;     //过电流检测
 static unsigned int evLast = 0xFFFF;     // 悬空端电压
 static unsigned int cv = 0;     // 过0时间统计值
 static unsigned int cv1 = 0;     // 过0时间统计值
@@ -94,7 +95,6 @@ static int dError=0,Error=0;
 
 void nextStep(){
 	if (dir ==1){
-			P14=~P14;
 		workstep++;
 		if(workstep >=6 ) workstep = 0;
 	}
@@ -164,10 +164,10 @@ void ioConf(){
 	P05_Input_Mode;		// AIN4 EA
 	clr_P05DIDS;
 
-	P07_Input_Mode;		// AIN2 EB
+	P07_Input_Mode;		// AIN3 EB
 	clr_P07DIDS;
 
-	P06_Input_Mode;	 // AIN3 EC
+	P06_Input_Mode;	 // AIN2 EC
 	clr_P06DIDS;
 
 	P17_Input_Mode;		// 中点
@@ -176,7 +176,7 @@ void ioConf(){
 	P04_Input_Mode;		// 速度控制 sc speed control _5V分压
 	clr_P17DIDS;
 
-	P11_Input_Mode;		// 过电流检测 cs current sence
+	P11_Input_Mode;		// 过电流检测 cs current sence AIN7
 	clr_P11DIDS;
 
 	P30_Input_Mode;		// 方向转换 hv 
@@ -184,16 +184,6 @@ void ioConf(){
 
 	P14_PushPull_Mode; 			// downC
 	clr_P14;
-	/*
-	   EA   P30 AIN1 -> P05 AIN4
-	   EB   P07 AIN2
-	   EC   P06 AIN3
-	   MV   P17 AIN0
-
-	   HV   (P17 AIN0) -> P30 AIN1
-	   CS   P11 AIN7
-	   SC   P04 AIN5
-	   */
 
 }
 
@@ -227,7 +217,7 @@ void setDuty(){
 
 void setCommutation(){
 	demag = 0;  evLast = 0xffff;         //消磁检测参考
-	joSwitch = 0; Enable_ADC_AIN0;  //第次换相以取中点电压为最先
+	adcSwitch = 0; Enable_ADC_AIN0;  //每次换相以取中点电压为最先
 	if ( workstep != 3 && workstep != 4)
 		DOWN_A_OFF;
 	if ( workstep != 0 && workstep != 5)
@@ -291,7 +281,7 @@ unsigned char  bldcStart(){
 
 void keepAllOff(){
 	//clr_ET3;
-	clr_ADCEN;       
+	clr_ADCEN;
 	clr_TR0; clr_TR1; clr_TR3; clr_ET3;
 	clr_P12; clr_P01; clr_P10; 	// upA  upB  upC OFF
 	PWMOUT2 = 0; setDuty(); 	// keep low for PWM mode start
@@ -300,7 +290,7 @@ void keepAllOff(){
 }
 
 void keepUpAllOff(){
-	clr_ADCEN;       
+	clr_ADCEN;
 	clr_TR0; clr_TR1; clr_TR3;
 	clr_P12; clr_P01; clr_P10; 	// upA  upB  upC OFF
 	PWMOUT2 = 0; setDuty(); 	// keep low for PWM mode start
@@ -308,7 +298,7 @@ void keepUpAllOff(){
 }
 
 void reKeepUpAllOff(){
-	set_ADCEN;       
+	set_ADCEN;
 	set_TR0; set_TR1; set_TR3;
 	// 其它项换相中自动设置
 }
@@ -330,19 +320,19 @@ void adcConf(){
 void reAIN(){
 	switch (workstep){
 		case 0:	                  // AB
-			Enable_ADC_AIN3; 
+			Enable_ADC_AIN2;  // ->2
 			break;
 		case 1:                            // AC
-			Enable_ADC_AIN2; 
+			Enable_ADC_AIN3;  // ->3
 			break;
 		case 2:                            // BC
 			Enable_ADC_AIN4; 
 			break;
 		case 3:                            // BA
-			Enable_ADC_AIN3; 
+			Enable_ADC_AIN2;  // ->2
 			break;
 		case 4:                            // CA
-			Enable_ADC_AIN2; 
+			Enable_ADC_AIN3;  // ->3
 			break;
 		case 5:                            // CB
 			Enable_ADC_AIN4; 
@@ -358,11 +348,10 @@ void forPid() interrupt 16 {
 
 // 定时1器溢出，用于换相延迟
 void forCommuation() interrupt 3 {
-	//workstep++; if(workstep >=6 ) workstep = 0;
-	//nextStep();
-	//setCommutation();
-	//set_ADCEX;      //继续pwm触发ADC
-	//clr_ADCF;      // 只有在没有过0时，才会启用ADC；当过0后，还未换相前不启用ADC
+	nextStep();
+	setCommutation();
+	set_ADCEX;      //继续pwm触发ADC
+	clr_ADCF;      // 只有在没有过0时，才会启用ADC；当过0后，还未换相前不启用ADC
 }
 
 // 上升反电势过0检查
@@ -381,7 +370,7 @@ void eUpChk(){
 // 下降反电势过0检查
 void eDownChk(){
 	if(demag == 0) {
-		if(ev > 0) demag = 1;
+		if(ev > 350) demag = 1;   //350 =0.4v 消磁的上限
 	}
 	else if( ev < hv ) {
 		zOk = 1;
@@ -396,14 +385,15 @@ void setDir(){
 }
 
 void setSpeed(){
-	if (hv < 819)                            // <1v 819
+	if (hv == 0)                            // <1v 819
 		Target = 800;
-	else if (hv > 819 && hv < 1638)          // <2v 1638
+	else if (hv > 1638 && hv < 2457)          // 2 （1638） ~ 3v （2457）
 		Target = 1000;
-	else if (hv > 1638 && hv < 2457)          // <3v 2457
+	else if (hv > 2457 && hv < 3276)          // 3 ~ 4v （3276）
 		Target = 1200;
-	else if (hv > 2457 && hv < 3276)          // 4v 3276
+	else if (hv >= 3276)          // >= 4v (3276)
 		Target = 1400;
+
 }
 
 /* 中断－ADC转换完成 [一直使能]  */
@@ -411,16 +401,16 @@ void adcHandle() interrupt 11 {
 	unsigned int t1pv = 0;                      // timer1初值
 	switch (startLevel) {
 		case 0: {              // 定方向
-				startLevel = 1;
+        startLevel = 1;    // 下一次取5v分压 speed control
+				Enable_ADC_AIN5;
 				hv = ADCRH; hv <<= 4; hv |= (ADCRL & 0X0F);  // 得HV分压值
 				setDir();
-				Enable_ADC_AIN5; // 下一次取5v分压 speed control
 				clr_ADCF;
 				set_ADCS;
 				break;
 			}
 		case 1: {             // 定speed
-				startLevel = 2;
+        startLevel = 2;     // 下一次为检过0,某一项ev
 				hv = ADCRH; hv <<= 4; hv |= (ADCRL & 0X0F);  // 得5v分压值
 				setSpeed();
 				clr_EADC;  // 不再启动ADC，停用ADC中断
@@ -428,19 +418,19 @@ void adcHandle() interrupt 11 {
 				clr_ADCF;
 				break;
 			}
-		case 2: {
-				//过0换相
-				set_P14;
-				zOk = 0;                 // 只要进入ADC就是为了进入新的过0点检测
-				/* ------------ PWM ON 方案 -----*/
-				if (joSwitch == 0)		// 先测中点
+   case 2: {             //过0换相, 与过电流检测
+     zOk = 0;                 // 重置为未过0 // 只有在没有过0时，才会启用ADC；当过0后，还未换相前不启用ADC
+
+				if (adcSwitch == 0) // 第1测中点电压
 				{
-					reAIN();	// 备下一步取悬空端电压
+					reAIN();
 					hv = ADCRH; hv <<= 4; hv |= (ADCRL & 0X0F);  // 得中点值
-					clr_ADCF;       // 只有在没有过0时，才会启用ADC；当过0后，还未换相前不启用ADC
 				}
-				else {					// 再测悬空端点
-					Enable_ADC_AIN0; // 备下一步取中点电压
+
+				else if (adcSwitch == 1) 			// 第2测端电压(ev)
+        {
+					//Enable_ADC_AIN0; // 如果过0 由换相函数设置取中点电压，未过0则由过电流部分设置AIN0
+          Enable_ADC_AIN7; // cs
 					ev = ADCRH; ev <<= 4; ev |= (ADCRL & 0X0F);
 					switch (workstep){
 						case 1: case 3: case 5:   //  反电势上升
@@ -452,6 +442,7 @@ void adcHandle() interrupt 11 {
 							else eUpChk();
 							break;
 					}
+          // 有过0  注： 只有在没有过0时，才会启用ADC；当过0后，还未换相前不启用ADC
 					if ( zOk == 1) {
 						clr_ADCEX;  //过0点后暂停PWM触发ADC
 						// 得到过0点间的定时值
@@ -466,12 +457,32 @@ void adcHandle() interrupt 11 {
 						set_TR1;                        // 让timer1去换相  
 						set_TR0;                        // 继续过0计时
 					}
-					else {                         // 没有产生过0
-						clr_ADCF;     // 只有在没有过0时，才会启用ADC；当过0后，还未换相前不启用ADC
-					}
+          // 没有过0 本case  break之前 cle_ADCF
 				}
-				joSwitch = ~joSwitch;  // 取ev电压和hv中点电压，交互进行;每次换相时，初始化为0,即先取中点
 
+        // 第3测过电流
+				else if (adcSwitch == 2)
+        {
+          //adcSwitch = 0; Enable_ADC_AIN0;  // 每检完总电流后，接着测中点电压
+          Enable_ADC_AIN0;  //中点
+					ocpv = ADCRH; ocpv <<= 4; ocpv |= (ADCRL & 0X0F);
+          if( ocpv > 102) {                // 如果过电流关闭一切 (102 = 0.125v / 2.5A@50mO)
+            keepAllOff();
+          }
+        }
+
+        // ------------------------ adcSwitch 变更 ----------------------
+				//adcSwitch = ~adcSwitch;  // 取ev电压和hv中点电压，交互进行;每次换相时，初始化为0,即先取中点
+        if (adcSwitch == 0){
+          adcSwitch = 1;
+        }
+        else if(adcSwitch == 1){
+          adcSwitch = 2;
+        }
+        else if(adcSwitch == 2){
+          adcSwitch = 0;
+        }
+        clr_ADCF;
 				break;
 			}
 	}
@@ -528,13 +539,12 @@ void main(){
 	}
 
 	/*系统配置检验*/
-/*
 	Enable_ADC_AIN1; // HV for dir信号检测 (startLevel =0)
 	set_EADC;
 	set_EA;
 	clr_ADCF;
 	set_ADCS;        // interrupt 11 去执行中断处理....
-*/
+
 	/*定位*/
 	preLoc(); 		// 内含duty设置
 	pwmStart();
